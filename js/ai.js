ef0c106e1ca33bf874025952ee5ac2c9f39bcf4a
Developer Ujjal <ujjalchandra213@gmail.com>
2024-09-20 09:33:21 +0600

    ai universe api

--- js/ai.js
+++ js/ai.js
@@ -21,6 +21,7 @@ const displayTools = (tools, isShowAll) => {
 
 
     tools.forEach(tool => {
+
         const newDiv = document.createElement('div');
         newDiv.classList.add('singel-ai-card');
         newDiv.innerHTML = `
@@ -45,7 +46,7 @@ const displayTools = (tools, isShowAll) => {
                                         <i class="material-icons">date_range</i> <span>${tool.published_in}</span>
                                     </div>
                                 </div>
-                                <div class="arrow-right">
+                                <div onclick="displayModal(true); loadModalInfo('${tool.id}')" class="arrow-right">
                                     <i class="material-icons">east</i>
                                 </div>
                             </div>
@@ -63,6 +64,95 @@ const displayTools = (tools, isShowAll) => {
 // See More
 const displayAllToots = () => {
     toolsDataLoad(true);
+}
+
+
+
+// modal show
+const displayModal = (x) => {
+    const aiModal = document.getElementById('aiModal');
+    if(x){
+        aiModal.classList.remove('hidden');
+    }
+    else{
+        aiModal.classList.add('hidden');
+    }
+    
+}
+
+
+
+const loadModalInfo = async (id) => {
+    const fetchInfo = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`);
+    const dataPromise = await fetchInfo.json();
+    const data = dataPromise.data;
+    displayModalInfo(data);
+
+}
+
+
+const displayModalInfo = (featureInfo) => {
+
+   const modalMainElement = document.getElementById('modal-main');
+   modalMainElement.innerHTML = `
+   
+                        <span onclick="closeModal()" class="cross-icon">&times;</span>
+                        <div class="modal-content-img-container">
+                            <div class="modal-content-box">
+                                <div class="modal-des">
+                                    <p>${featureInfo.description}</p>
+                                </div>
+                                <div class="modal-price-container">
+                                    <div class="bc-price basic-price">
+                                        <span class="one">${featureInfo.pricing[0].price} <span>${featureInfo.pricing[0].plan}</span></span>
+                                    </div>
+                                    <div class="pro-price basic-price">
+                                        <span>${featureInfo.pricing[1].price} <span>${featureInfo.pricing[1].plan}</span></span>
+                                    </div>
+                                    <div class="enterprise-price basic-price">
+                                        <span>${featureInfo.pricing[2].price} <span>${featureInfo.pricing[2].plan}</span></span>
+                                    </div>
+                                </div>
+                                <div class="modal-feature-inte-container">
+                                    <div class="feature">
+                                        <h3>Features</h3>
+                                        <ul>
+                                            <li>${featureInfo?.features['1']?.feature_name}</li>
+                                            <li>${featureInfo?.features['2']?.feature_name}</li>
+                                            <li>${featureInfo?.features['3']?.feature_name}</li>
+                                        </ul>
+                                    </div>
+                                    <div class="feature">
+                                        <h3>Integrations</h3>
+                                        <ul>
+                                            <li>${featureInfo?.integrations[0]}</li>
+                                            <li>${featureInfo?.integrations[1]}</li>
+                                            <li>${featureInfo?.integrations[2]}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-img-box">
+                                <div class="modal-img">
+                                    <img src="${featureInfo?.image_link[0] }" alt="">
+                                    <div class="accuracy">
+                                        <p><span>${featureInfo?.accuracy?.score}</span> accuracy</p>
+                                    </div>
+                                </div>
+                                <div class="modal-img-des">
+                                    <h3>${featureInfo?.input_output_examples[0]?.input}</h3>
+                                    <p>${featureInfo?.input_output_examples[0]?.output}</p>
+                                </div>
+                            </div>
+                        </div>
+   
+   `;
+}
+
+
+//Model close
+const closeModal = () => {
+    displayModal(false)
 }
 
 
---
+++ practice.js
@@ -0,0 +1,10 @@
+const obj ={
+    1:"ami",
+    roll: 55
+}
+
+obj.roll = 55;
+
+obj[1] = 30;
+
+console.log(obj)
